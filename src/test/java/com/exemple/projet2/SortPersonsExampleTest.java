76427f646fdb134655d5468d09bba77cb198e16c
fatima ezzahra <adblafatmtalzhra@gmail.com>
2024-12-22 17:44:03 +0100

    remove puplic static void main

--- src/test/java/com/exemple/projet2/SortPersonsExampleTest.java
+++ src/test/java/com/exemple/projet2/SortPersonsExampleTest.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
-public class SortPersonsExample {
-    public static void main(String[] args) {
+public class SortPersonsExampleTest {
+
+    @Test
+    public void testSortPersons() {
+
         List<Person> people = new ArrayList<>();
         people.add(new Person(" Hamid ", " Jamila "));
         people.add(new Person(" Martin ", " Bob "));
@@ -15,14 +18,10 @@ public class SortPersonsExample {
         people.add(new Person(" Bernard ", " Jamila "));
 
         Collections.sort(people);
+
         assertEquals("Bernard, Jamila", people.get(0).toString());
         assertEquals("Hamid, Charles", people.get(1).toString());
         assertEquals("Hamid, Jamila", people.get(2).toString());
         assertEquals("Martin, Bob", people.get(3).toString());
-
-        System.out.println(" Liste tri é e de personnes : ");
-        for (Person person : people) {
-            System.out.println(person);
-        }
     }
 }
---
+++ src/test/java/com/exemple/projet2/Test.java
@@ -0,0 +1,5 @@
+package com.exemple.projet2;
+
+public @interface Test {
+
+}
